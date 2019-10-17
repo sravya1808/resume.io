bd05948a31402019933f31e6e70c09cb1fa613b2
sravya1808 <52944842+sravya1808@users.noreply.github.com>
2019-10-17 23:33:56 +0530

    Create servlet.java

---
+++ servlet.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class servlethello extends HttpServlet {
+ 
+   private String message;
+
+   public void init() throws ServletException {
+      
+      message = "Hello World";
+   }
+
+   public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+      
+     
+      response.setContentType("text/html");
+
+      
+      PrintWriter out = response.getWriter();
+      out.println("<h1>" + message + "</h1>");
+   }
+
+  
+}
